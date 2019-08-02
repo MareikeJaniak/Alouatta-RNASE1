c89bc88a22ff16b12b006a2e943b216a3caa545f
MareikeJaniak <30602072+MareikeJaniak@users.noreply.github.com>
2019-08-02 14:21:11 -0600

    Update RNASE1_mapping_23May19_consensus.sh

--- scripts/RNASE1_mapping_23May19_consensus.sh
+++ scripts/RNASE1_mapping_23May19_consensus.sh
@@ -3,11 +3,11 @@ fastq="/home/mareike.janiak/data/trimmed_fastq"                 #create variable
 bwadir="/home/mareike.janiak/data/Alouatta_mapping/mapping/bwa_out"     #create variable for output directory
 
 while read line;do
-        mkdir -p $bwadir/sbatch/;         #make directory for qsub files created below
+        mkdir -p $bwadir/sbatch/;         #make directory for sbatch files created below
         mkdir -p $bwadir/errors/;       #make directory for error files
         mkdir -p $bwadir/stdout/;       #make directory for standard out files
 
-#what to write to individual qsub files
+#what to write to individual sbatch files
         echo "#!/bin/sh
 #SBATCH -t 0-08:00:00
 #SBATCH -N 1 
@@ -17,6 +17,4 @@ while read line;do
 #SBATCH -o /home/mareike.janiak/data/Alouatta_mapping/mapping/bwa_out/stdout/$line.consensus.out
 
 bwa mem -M $ref $fastq/${line}_R1_001.fastq.gz $fastq/${line}_R2_001.fastq.gz -t4 | samtools view -@4 -Sbh - | samtools sort -@4 -T $bwadir/Apal_10X/${line}.consensus.sort.tmp -O bam -o - > $bwadir/Apal_10X/${line}.consensus.sort.bam; samtools index $bwadir/Apal_10X/${line}.consensus.sort.bam" > $bwadir/sbatch/$line.bwa.sbatch; sbatch $bwadir/sbatch/$line.bwa.sbatch; done < $fastq/fastq_files.txt
-
-#use bwa mem function to map paired read files to consensus RNASE1 region. Each pair of fastq files is mapped, sorted and indexed. List of fastq file names to input is in fastq_files.txt.
 
